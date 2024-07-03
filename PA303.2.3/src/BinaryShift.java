7eb060db1575a6c49e45acff743aaa3b6cb83fed
jsalicetti <jessicasalicetti@yahoo.com>
2024-07-03 09:28:17 -0400

    problem 2 and 3 solved

--- PA303.2.3/src/BinaryShift.java
+++ PA303.2.3/src/BinaryShift.java
@@ -30,7 +30,52 @@ public class BinaryShift {
         System.out.println(Integer.toBinaryString(a));
         System.out.println(Integer.toBinaryString(b));
         System.out.println(Integer.toBinaryString(c));
+
+// pt2
+
+        // declares a variable x and assigns 150 to it
+        int x2 = 150;
+
+        // prints out the binary string version of the number
+        System.out.println(Integer.toBinaryString(x2));
+        // use shift operator to shift by 2 and assign the result to x
+        x2 = x2 >> 1;
+
+        System.out.println(x2);
+        System.out.println(Integer.toBinaryString(x2));
+
+        // second part
+
+        int d = 225;
+        int e = 1555;
+        int f = 32456;
+
+        System.out.println(Integer.toBinaryString(d));
+        System.out.println(Integer.toBinaryString(e));
+        System.out.println(Integer.toBinaryString(f));
+
+        d = d >> 1;
+        e = e >> 1;
+        f = f >> 1;
+
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+        System.out.println(Integer.toBinaryString(d));
+        System.out.println(Integer.toBinaryString(e));
+        System.out.println(Integer.toBinaryString(f));
+
+        // pt3
+
+        int x1 = 7;
+        int y = 17;
+
+        int z = x1 & y;
+
+        System.out.println(Integer.toBinaryString(x1));
+        System.out.println(Integer.toBinaryString(y));
+        System.out.println(Integer.toBinaryString(z));
+
     }
 }
-
-
